d0f72d46ec34f2e978e2e778eb8d9d47a399c3ea
mendelsshop <mendelsshop@gmail.com>
2025-03-21 16:48:45 -0400

    added lattice operations

---
+++ lib/lattice.ml
@@ -0,0 +1,72 @@
+module type T = sig
+  type t
+
+  val bottom : t
+  val top : t
+  val leq : t -> t -> bool
+  val eq : t -> t -> bool
+  val join : t -> t -> t
+  val meet : t -> t -> t
+end
+
+module Number = struct
+  type t = Integer of int | PInfinity | NInfinity
+
+  let leq l r =
+    match (l, r) with
+    | NInfinity, _ | _, PInfinity -> true
+    | Integer l, Integer r -> l <= r
+    | _ -> false
+
+  let eq l r =
+    match (l, r) with
+    | NInfinity, NInfinity | PInfinity, PInfinity -> true
+    | Integer l, Integer r -> l <= r
+    | _ -> false
+
+  let min l r =
+    match (l, r) with
+    | NInfinity, _ | _, NInfinity -> NInfinity
+    | l, PInfinity | PInfinity, l -> l
+    | Integer l, Integer r -> Integer (min l r)
+
+  let max l r =
+    match (l, r) with
+    | NInfinity, l | l, NInfinity -> l
+    | _, PInfinity | PInfinity, _ -> PInfinity
+    | Integer l, Integer r -> Integer (max l r)
+end
+
+module Interval : T = struct
+  type t = Interval of Number.t * Number.t | Bottom
+
+  let bottom = Bottom
+  let top = Interval (NInfinity, PInfinity)
+
+  let leq l r =
+    match (l, r) with
+    | Bottom, _ -> true
+    | _, Bottom -> false
+    | Interval (l1, l2), Interval (r1, r2) ->
+        Number.leq l1 r1 && Number.leq l2 r2
+
+  let eq l r =
+    match (l, r) with
+    | Bottom, Bottom -> true
+    | Interval (l1, l2), Interval (r1, r2) -> Number.eq l1 r1 && Number.eq l2 r2
+    | _ -> false
+
+  let join l r =
+    match (l, r) with
+    | i, Bottom | Bottom, i -> i
+    | Interval (l1, l2), Interval (r1, r2) ->
+        Interval (Number.min l1 r1, Number.max l2 r2)
+
+  let meet l r =
+    match (l, r) with
+    | _, Bottom | Bottom, _ -> Bottom
+    | Interval (l1, l2), Interval (r1, r2) ->
+        let max = Number.max l1 r1 in
+        let min = Number.min l2 r2 in
+        if Number.leq max min then Interval (max, min) else Bottom
+end
