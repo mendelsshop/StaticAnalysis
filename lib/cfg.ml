7d3d0ada508c7dca7a5a316173e728522c76ded5
mendelsshop <mendelsshop@gmail.com>
2025-03-25 23:56:33 -0400

    Addded CFG represetnation

---
+++ lib/cfg.ml
@@ -0,0 +1,49 @@
+type identifier = Identifier of string
+type basic_expr = Integer of int | Boolean of bool | Identifier of identifier
+type unary_operator = Not | Negate | AbsoluteValue
+type node_reference = Node of int
+
+type binary_operator =
+  | Add
+  | Subtract
+  | Multiply
+  | Divide
+  | And
+  | Or
+  | LessThen
+  | GreaterThan
+  | LessThenOrEqual
+  | GreaterThanOrEqual
+  | Equal
+  | NotEqual
+
+type expr =
+  | Basic of basic_expr
+  | UnaryOperator of { operator : unary_operator; operand : basic_expr }
+  | BinaryOperator of {
+      left : basic_expr;
+      operator : binary_operator;
+      right : basic_expr;
+    }
+
+type command =
+  | Assign of { target : identifier; value : expr }
+  | Variable of identifier
+  | Cond of expr
+
+type edge = Directed | True | False
+type node = { id : int; command : command }
+
+module Node = struct
+  type t = node
+
+  let compare { id = id1; _ } { id = id2; _ } = Int.compare id1 id2
+end
+
+module NodeMap = Map.Make (Node)
+
+type graph = {
+  nodes : node list;
+  successors : (edge * node) NodeMap.t;
+  predecesseors : (edge * node) NodeMap.t;
+}
