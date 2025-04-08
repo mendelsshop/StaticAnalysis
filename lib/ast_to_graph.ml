b0da1670fe6f5cd7a6af9871f5f277358393a1c3
mendelsshop <mendelsshop@gmail.com>
2025-04-08 12:48:56 -0400

    more ast to graph continuation stuff

--- lib/ast_to_graph.ml
+++ lib/ast_to_graph.ml
@@ -6,70 +6,67 @@ let gensym r =
 let rec append list x k =
   match list with [] -> k x | i :: is -> i :: append is x k
 
-let rec expr_to_simple_bool_expr (expr : Ast.expression) g =
-  let apply_op r l op : 'a list * Cfg.basic_bool_expr =
-    let stmts, l' = expr_to_simple_bool_expr l g in
-    let stmts', r' = expr_to_simple_bool_expr r g in
+let rec expr_to_simple_bool_expr (expr : Ast.expression) g :
+    Cfg.basic_bool_expr * (Cfg.command list -> Cfg.command list) =
+  let apply_op r l op :
+      Cfg.basic_bool_expr * (Cfg.command list -> Cfg.command list) =
+    let l', stmts = expr_to_simple_bool_expr l g in
+    let r', stmts' = expr_to_simple_bool_expr r g in
     let temp = gensym g in
-
-    ( stmts @ stmts'
-      @ [
-          Cfg.AssignBool
-            {
-              target = Identifier temp;
-              value =
-                Cfg.BinaryOperator { operator = op; left = l'; right = r' };
-            };
-        ],
-      Cfg.Identifier (Identifier temp) )
+    let command =
+      Cfg.AssignBool
+        {
+          target = Identifier temp;
+          value = Cfg.BinaryOperator { operator = op; left = l'; right = r' };
+        }
+    in
+    (Cfg.Identifier (Identifier temp), fun s -> command :: s |> stmts' |> stmts)
   in
   match expr with
-  | Ast.Variable (v, _ty) -> ([], Cfg.Identifier (Identifier v))
+  | Ast.Variable (v, _ty) -> (Cfg.Identifier (Identifier v), Fun.id)
   | Ast.Number _ -> failwith "not reachable"
-  | Ast.Boolean b -> ([], Cfg.Boolean b)
+  | Ast.Boolean b -> (Cfg.Boolean b, Fun.id)
   | Ast.Math (_, _, _) -> failwith "not reachable"
   | Ast.UnaryMath (_, _) -> failwith "not reachable"
-  | Ast.Compare (_l, _o, _r) ->
-      (*let stmts, l' = expr_to_simple_int_expr l g in*)
-      (*let stmts', r' = expr_to_simple_int_expr r g in*)
-      (*let temp = gensym g in*)
-      failwith ""
-      (*( stmts @ stmts'*)
-      (*  @ [*)
-      (*      Cfg.AssignBool*)
-      (*        {*)
-      (*          target = Identifier temp;*)
-      (*          value =*)
-      (*            Cfg.Compare*)
-      (*              {*)
-      (*                operator =*)
-      (*                  (match o with*)
-      (*                  | Ast.Greater -> Cfg.GreaterThan*)
-      (*                  | Ast.Less -> Cfg.LessThen*)
-      (*                  | Ast.LessOrEqual -> Cfg.LessThenOrEqual*)
-      (*                  | Ast.GreaterOrEqual -> Cfg.GreaterThanOrEqual*)
-      (*                  | Ast.Equal -> Cfg.Equal*)
-      (*                  | Ast.NotEqual -> Cfg.NotEqual);*)
-      (*                left = l';*)
-      (*                right = r';*)
-      (*              };*)
-      (*        };*)
-      (*    ],*)
-      (*  Cfg.Identifier (Identifier temp) )*)
+  | Ast.Compare (l, o, r) ->
+      let l', stmts = expr_to_simple_int_expr l g in
+      let r', stmts' = expr_to_simple_int_expr r g in
+      let temp = gensym g in
+      let command =
+        Cfg.AssignBool
+          {
+            target = Identifier temp;
+            value =
+              Cfg.Compare
+                {
+                  operator =
+                    (match o with
+                    | Ast.Greater -> Cfg.GreaterThan
+                    | Ast.Less -> Cfg.LessThen
+                    | Ast.LessOrEqual -> Cfg.LessThenOrEqual
+                    | Ast.GreaterOrEqual -> Cfg.GreaterThanOrEqual
+                    | Ast.Equal -> Cfg.Equal
+                    | Ast.NotEqual -> Cfg.NotEqual);
+                  left = l';
+                  right = r';
+                };
+          }
+      in
+      ( Cfg.Identifier (Identifier temp),
+        fun s -> command :: s |> stmts' |> stmts )
   | Ast.And (l, r) -> apply_op r l Cfg.And
   | Ast.Or (l, r) -> apply_op r l Cfg.Or
   | Ast.Not b ->
-      let stmts, b' = expr_to_simple_bool_expr b g in
+      let b', stmts = expr_to_simple_bool_expr b g in
       let temp = gensym g in
-      ( stmts
-        @ [
-            Cfg.AssignBool
-              {
-                target = Identifier temp;
-                value = Cfg.UnaryOperator { operator = Cfg.Not; operand = b' };
-              };
-          ],
-        Cfg.Identifier (Identifier temp) )
+      let command =
+        Cfg.AssignBool
+          {
+            target = Identifier temp;
+            value = Cfg.UnaryOperator { operator = Cfg.Not; operand = b' };
+          }
+      in
+      (Cfg.Identifier (Identifier temp), fun s -> command :: s |> stmts)
 
 and expr_to_simple_int_expr (expr : Ast.expression) g :
     Cfg.basic_int_expr * (Cfg.command list -> Cfg.command list) =
@@ -95,7 +92,7 @@ and expr_to_simple_int_expr (expr : Ast.expression) g :
                 };
           }
       in
-      (Cfg.Identifier (Identifier temp), fun s -> stmts (List.cons command s))
+      (Cfg.Identifier (Identifier temp), fun s -> command :: s |> stmts)
   | Ast.Math (l, o, r) ->
       let l', stmts = expr_to_simple_int_expr l g in
       let r', stmts' = expr_to_simple_int_expr r g in
@@ -120,7 +117,7 @@ and expr_to_simple_int_expr (expr : Ast.expression) g :
           }
       in
       ( Cfg.Identifier (Identifier temp),
-        fun s -> stmts (stmts' (List.cons command s)) )
+        fun s -> command :: s |> stmts' |> stmts )
   | Ast.Compare (_, _, _) -> failwith "not reachable"
   | Ast.And (_, _) -> failwith "not reachable"
   | Ast.Or (_, _) -> failwith "not reachable"
